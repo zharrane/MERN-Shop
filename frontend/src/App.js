50b3f7f7e202e403e81665eae58ea19532ca9455
unknown <zakariaharrane2@gmail.com>
2021-04-02 15:47:50 +0100

    second commit just simple modifications

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import Footer from './components/Footer'
+import Header from './components/Header'
 
-const  App =()=> {
+const App = () => {
   return (
     <>
-      <h1>Welcome to MERN SHOP</h1>
+      <Header />
+      <main>
+        <Container>
+          <h1>Welcome to MERN Shop</h1>
+        </Container>
+      </main>
+
+      <Footer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+const Footer = () => {
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col classname='text-center py-3'></Col>CopyRight &copy; Mern Shop
+          Zharrane
+        </Row>
+      </Container>
+    </footer>
+  )
+}
+
+export default Footer
